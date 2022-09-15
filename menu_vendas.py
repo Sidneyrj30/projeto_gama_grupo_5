f1081e582e3c5e3eaae185d14e4b4e1080e82b8f
Sidney Júnior <sidneyrj45@gmail.com>
2022-09-15 09:56:24 -0300

    verficacao quatidade do carrinho e produtos

--- menu_vendas.py
+++ menu_vendas.py
@@ -30,33 +30,35 @@ def menu_vendas(produtos, carrinho, relatorio):
                     Adicionar produtos        
             -------------------------------- 
             ''')
-            print("")
-            for item in range(len(produtos)):
-                print(f"[{item + 1}] - {produtos[item]['nome']}: R$ {produtos[item]['preco']:.2f}")
 
-            valor_valido = False
-            while valor_valido == False:
-                produto = input("\nEscolha o produto que deseja adicionar no carrinho: ")
-                valor_valido = produto.isnumeric()
-                if valor_valido == True:
-                    produto_num = int(produto)
-                    if (produto_num - 1) in range(len(produtos)):
-                        carrinho.append(produtos[produto_num - 1])
-                        del produtos[produto_num - 1]
+            if len(produtos) == 0:
+                print("Não tem produtos disponível")
+                input("\nAperte enter para continuar... ")
+            else:
+                print("")
+                for item in range(len(produtos)):
+                    print(f"[{item + 1}] - {produtos[item]['nome']}: R$ {produtos[item]['preco']:.2f}")
 
-                        print(f"\nSua lista do carrinho é:")
-                        for item in range(len(carrinho)):
-                            print(f"{carrinho[item]['nome']}")
-                    elif len(produtos) == 0:
-                        print("Não tem mais produtos")
-                        valor_valido = True
+                valor_valido = False
+                while valor_valido == False:
+                    produto = input("\nEscolha o produto que deseja adicionar no carrinho: ")
+                    valor_valido = produto.isnumeric()
+                    if valor_valido == True:
+                        produto_num = int(produto)
+                        if (produto_num - 1) in range(len(produtos)):
+                            carrinho.append(produtos[produto_num - 1])
+                            del produtos[produto_num - 1]
+
+                            print(f"\nSua lista do carrinho é:")
+                            for item in range(len(carrinho)):
+                                print(f"{carrinho[item]['nome']}")
+                        else:
+                            print("Você digitou um produto inexistente")
+                            valor_valido = False
                     else:
                         print("Você digitou um produto inexistente")
-                        valor_valido = False
-                else:
-                    print("Você digitou um produto inexistente")
-            
-            input("\nAperte enter para continuar... ")
+                
+                input("\nAperte enter para continuar... ")
 
         elif opcao == "2":
             os.system('cls')
@@ -66,34 +68,35 @@ def menu_vendas(produtos, carrinho, relatorio):
                     Remover produtos do carrinho       
             ------------------------------------------ 
             ''')
-            
-            print("")
-            for item in range(len(carrinho)):
-                print(f"{item + 1}-{carrinho[item]['nome']}: R${carrinho[item]['preco']:.2f}")
 
-            valor_valido = False
-            while valor_valido == False:
-                produto = input("\nEscolha o produto que deseja remover no carrinho: ")
-                valor_valido = produto.isnumeric()
-                if valor_valido == True:
-                    produto_num = int(produto)
-                    if (produto_num - 1) in range(len(carrinho)):
-                        produtos.append(carrinho[produto_num - 1])
-                        del carrinho[produto_num - 1]
+            if len(carrinho) == 0:
+                print("Você não tem produtos no carrinho")
+                input("\nAperte enter para continuar... ")
+            else:                                    
+                print("")
+                for item in range(len(carrinho)):
+                    print(f"{item + 1}-{carrinho[item]['nome']}: R${carrinho[item]['preco']:.2f}")
 
-                        print(f"\nSua lista do carrinho é:")
-                        for item in range(len(carrinho)):
-                            print(f"{carrinho[item]['nome']}")
-                    elif len(carrinho) == 0:
-                        print("Não tem produtos no carrinho")
-                        valor_valido = True
+                valor_valido = False
+                while valor_valido == False:
+                    produto = input("\nEscolha o produto que deseja remover no carrinho: ")
+                    valor_valido = produto.isnumeric()
+                    if valor_valido == True:
+                        produto_num = int(produto)
+                        if (produto_num - 1) in range(len(carrinho)):
+                            produtos.append(carrinho[produto_num - 1])
+                            del carrinho[produto_num - 1]
+
+                            print(f"\nSua lista do carrinho é:")
+                            for item in range(len(carrinho)):
+                                print(f"{carrinho[item]['nome']}")
+                        else:
+                            print("Você digitou um produto inexistente")
+                            valor_valido = False
                     else:
                         print("Você digitou um produto inexistente")
-                        valor_valido = False
-                else:
-                    print("Você digitou um produto inexistente")
-                       
-            input("\nAperte enter para continuar... ")
+                        
+                input("\nAperte enter para continuar... ")
 
         elif opcao == "3":
             os.system('cls') 
