cdee115193147ce699abeaf9d30c67e76b7efdd3
Bruno Fernandes <brunocosta@luizalabs.com>
2022-09-15 12:38:56 -0300

    menu cadastro atualizado
    - Tratado alguns erros encontrados

--- menu_cadastro.py
+++ menu_cadastro.py
@@ -1,83 +1,85 @@
-
 import os
-produtos = [{'nome':"arroz", 'preco': 15.00}, {'nome':"feijao",'preco': 20.00}, {'nome':"carne", 'preco': 30.00},
-{'nome':'cebola', 'preco': 5.56}]
-opcao = '1'
-while opcao != '0':
-    os.system('cls')
-    opcao=input('''
-    --------------------------------------------
-        Voce entrou no menu principal:
-    --------------------------------------------
-    [1] -Cadastro
-    [2] -Vendas
-    [3] -Relatório
-    [0] -Sair
+produtos = []
 
-    Entre com o numero da solicitação: ''')
+def menu_cadastro(produtos):
+    opcao = 0
 
-
-    while opcao != '0':
+    while opcao != "0":
         os.system('cls')
-        menu_cadastro=input('''
+        menu_cadastro = input('''
         --------------------------------------------
-                Voce entrou no menu de cadastro:
+                Voce entrou no menu de cadastro
         --------------------------------------------
+        
         [1] -Cadastramento de produtos
         [2] -Listar produtos cadastrados
         [3] -Deleção de produtos
         [0] -Sair para o menu principal
-        entre com o numero da solicitação ''')
 
-        if menu_cadastro == '1' :
+        Escolha uma das opções:  ''')
+        if menu_cadastro =='0':
+            break
+
+        if menu_cadastro == '1':
             os.system('cls')
             print('Voce entrou na aba de cadastramento de produtos:')
-            print(produtos)
-            #produto_cadastro ={}
-            produto={}
+            produto = {}
             produto_cadastro = input("Digite o nome do produto: ")
             while produto_cadastro.isalpha() == False:
                 produto_cadastro = input("Nome invalido digite o nome novamente: ")
             os.system('cls')
-            preco = input(f"Digite o preço de {produto_cadastro}: ")       #replace(",", ".")
-            while preco.isdecimal() == False:#preco.isdecimal() == False: #rever logo pela manha
+            preco = input(f"Digite o preço de {produto_cadastro}: ").replace(',','.',1)     
+            while preco.replace('.','',1).isdigit() == False:                
                 preco = input(f"Preço invalido digite novamente um preço para {produto_cadastro}: ")
-            produto[produto_cadastro]=preco
+
+            preco = float(preco)
+            produto['nome'] = produto_cadastro
+            produto['preco'] = preco
             produtos.append(produto)
-            print(f'\nProduto {produto_cadastro}, com valor de {preco}, cadastrado com sucesso!!')
+            print(f'\nProduto {produto_cadastro}, com valor de R${preco:.2f}, cadastrado com sucesso!!')
             input('\nPressione Enter para voltar ao menu ')
-            opcao == '1'
-       
             
 
-
         elif menu_cadastro == '2':
+            os.system('cls')
             print('Voce entrou na aba de Listagem de produtos')
             print('Seu estoque atual é \n')
-            contador=0
+            contador=1
             for item in range(len(produtos)):
-                print(f"produto {contador}: {produtos[item]['nome']}")
+                print(f"produto {contador}: {produtos[item]['nome']} R$ {produtos[item]['preco']:.2f}")
                 contador+=1
 
             input('\nDigite qualquer coisa para voltar ao menu ')
-            opcao == '1'
+            
 
         elif menu_cadastro == '3':
             os.system('cls')
             print('Voce entrou na aba de Deleção')
-            contador=0
-            for produto in produtos:
-                print(f'produto {contador}: {produto}')
+            contador=1
+            for item in range(len(produtos)) :
+                print(f"produto {contador}: {produtos[item]['nome']}")
                 contador+=1
-                
-                
+                                                
             print('qual produto deseja remover digite o numero correspondente:\n')
-            numero = int(input("Digite o numero do produto da lista "))
-            produtos.pop(numero)
-            print('Seu estoque atual é \n',produtos)
-            input('\nDigite qualquer coisa para voltar ao menu ')
-            opcao == '1'
+            
+            
+            numero=0
+            while (len(produtos)) == 0 and opcao!='0':
+                input('Lista vazia de Enter para voltar ao menu')
+                opcao=0
+                break
+            while numero-1 not in range(len(produtos)) and (len(produtos)) >0:
+                numero = int(input("Digite o numero do produto da lista "))
+                #numero = numero+1
 
-        else:
-            os.system('cls')
-            opcao='0'
+                            
+                numero = numero-1
+                produtos.pop(numero)
+                print('Seu estoque atual é \n')
+                contador = 1
+                for item in range(len(produtos)):
+                    print(f"produto {contador}: {produtos[item]['nome']}")
+                    contador +=1
+                input('\nDigite qualquer coisa para voltar ao menu ')
+                break   
+                   
